60bbf3cd58c5cc1678970f159a0cd9608a0a4b53
sdoyle0 <sdoyle0+github@gmail.com>
2019-05-26 10:34:25 -0400

    Removing need to serve JS files directly

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const app = express(),
 	HTML_FILE = path.join(DIST_DIR, 'index.html')
 app.use(express.static(DIST_DIR))
 app.use('/coherence-map', express.static(DIST_DIR));
-app.use('/coherence-map/bundle.css', express.static(path.join(DIST_DIR, 'build', 'bundle.css')));
-app.use('/coherence-map/browser-bundle.js', express.static(path.join(DIST_DIR, 'build', 'browser-bundle.js')));
 app.get('*', (req, res) => {
 	res.sendFile(HTML_FILE)
 });
